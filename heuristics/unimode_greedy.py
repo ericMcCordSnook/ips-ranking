7f5e0ee863362576e0fbe5526bd142c60472c0ed
Eric McCord-Snook <esm7ky@virginia.edu>
2018-09-20 13:40:41 -0400

    Starting to add class structure to libraries so that they can be used via configuration files

--- heuristics/unimode_greedy.py
+++ heuristics/unimode_greedy.py
@@ -1,23 +1,41 @@
-def get_greedy_traversal_order(graph, start_node):
-    order = [start_node]
-    visited = set()
-    visited.add(start_node)
-    current_nodes = graph[start_node]
-    i = 0
-    while bool(current_nodes): # tests if empty
-        i = i + 1
-        print("Step", i, current_nodes)
-        min_dist_node = None
-        min_dist = 100
-        for node, dist in current_nodes.items():
-            if dist < min_dist:
-                min_dist_node = node
-                min_dist = dist
-        visited.add(min_dist_node)
-        order.append(min_dist_node)
-        for node, dist in graph[min_dist_node].items():
-            if node not in visited:
-                if (node not in current_nodes) or (min_dist + dist < current_nodes[node]):
-                    current_nodes[node] = min_dist + min_dist
-        del current_nodes[min_dist_node]
-    return order
+from utils.miscutils import generate_weighted_permutation_graph
+from math import ceil, factorial
+import sys
+from copy import deepcopy
+
+# Try making a class that has all the heuristics objects then create generic
+# heuristics object by doing getattr(HeuristicsClass, "Unimode_Greedy")
+
+class Unimode_Greedy:
+    def get_greedy_traversal_order(self, graph, start_node):
+        order = [start_node]
+        visited = set()
+        visited.add(start_node)
+        current_nodes = deepcopy(graph[start_node])
+        while bool(current_nodes): # tests if empty
+            min_dist_node = None
+            min_dist = sys.maxsize
+            for node, dist in current_nodes.items():
+                if dist < min_dist:
+                    min_dist_node = node
+                    min_dist = dist
+            visited.add(min_dist_node)
+            order.append(min_dist_node)
+            for node, dist in graph[min_dist_node].items():
+                if node not in visited:
+                    if (node not in current_nodes) or (min_dist + dist < current_nodes[node]):
+                        current_nodes[node] = min_dist + min_dist
+            del current_nodes[min_dist_node]
+        return order
+
+    # All heuristics will have a method of the same name
+    def get_perms_to_explore(self, graph, mode_ranking, pct=0.2, num=None):
+        num_to_return = 0
+        if num is not None:
+            if not isinstance(num, int):
+                raise ValueError("num argument must be an integer")
+            else:
+                num_to_return = num
+        else:
+            num_to_return = int(ceil(pct * factorial(len(mode_ranking))))
+        return get_greedy_traversal_order(graph, mode_ranking)[:num_to_return]
--- run.py
+++ run.py
@@ -1,13 +1,15 @@
 from experiments.run_experiment import run
 from rankobjects.weight import *
 from utils.miscutils import generate_weighted_permutation_graph
-from heuristics.unimode_greedy import get_greedy_traversal_order
+from heuristics.unimode_greedy import get_greedy_traversal_order, get_perms_to_explore
 
 if __name__ == '__main__':
-    weight = Arithmetic(a=1.0,b=0.2)
-    graph = generate_weighted_permutation_graph(4,weight)
-    order = get_greedy_traversal_order(graph, (1,2,3,4))
-    print(order)
+    weight = Arithmetic(a=1.0,b=0.1)
+    graph = generate_weighted_permutation_graph(7,weight)
+    order = get_greedy_traversal_order(graph, (1,2,3,4,5,6,7))
+    print(order, "\n")
+    order2 = get_perms_to_explore(graph, (1,2,3,4,5,6,7), num=10)
+    print(order2)
 
 # if __name__ == '__main__':
 #     run()
