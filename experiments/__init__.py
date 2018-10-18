ded181a6f765885cdde8757f4841b899cea93191
Eric McCord-Snook <esm7ky@virginia.edu>
2018-10-18 01:31:15 -0400

    Ran experiments

--- experiments/__init__.py
+++ experiments/__init__.py
@@ -1,7 +1,5 @@
 from experiments.Generic_Experiment import Generic_Experiment
-from experiments.Single_Start_Greedy_Experiment import Single_Start_Greedy_Experiment
 
 __all__ = [
     'Generic_Experiment',
-    'Single_Start_Greedy_Experiment'
 ]
