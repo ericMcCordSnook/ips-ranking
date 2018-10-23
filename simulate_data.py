5d154505ba978851c6cf3f94f0cf7210cf254fd0
Eric McCord-Snook <esm7ky@virginia.edu>
2018-10-23 19:04:34 -0400

    Added ability to simulate data with arbitrarily many distributions with different ground truths, where each distribution has a specified weight, ran experiments on bimodal simulated data (see log files)

--- simulate_data.py
+++ simulate_data.py
@@ -1,6 +1,8 @@
 import sys
 import yaml
 from utils import Data_Simulator
+import logging
+import datetime
 
 def yaml_load(fp):
     with open(fp, "r") as fd:
@@ -8,6 +10,19 @@ def yaml_load(fp):
     return config_data
 
 if __name__ == '__main__':
-    config_data = yaml_load(sys.argv[1])
+    config_path = sys.argv[1]
+    # Filename is the after the last slash in the path
+        # the [:-4] removes the .yml
+    config_filename = config_path.split("/")[-1][:-4]
+    config_data = yaml_load(config_path)
+    log_output = "output/" + config_filename + ".log"
+    logging.basicConfig(filename=log_output,
+                        level=logging.INFO,
+                        format="%(message)s",
+                        filemode='w')
+    exp_datetime = datetime.datetime.now()
+    logging.info("%s\n" % exp_datetime)
+
+    config_data = yaml_load(config_path)
     simulator = Data_Simulator(config_data)
     simulator.simulate()
--- utils/Data_Simulator.py
+++ utils/Data_Simulator.py
@@ -1,17 +1,17 @@
 from rankobjects import *
 import numpy as np
 from itertools import permutations
+import logging
 
 class Data_Simulator:
 
     def __init__(self, data):
-        weight_types = {"Arithmetic": Arithmetic, "Geometric": Geometric}
+        self.weight_types = {"Arithmetic": Arithmetic, "Geometric": Geometric}
         self.num_elements = data["num_elements"]
         self.num_rankings = data["num_rankings"]
-        self.weight_a = data["weight_a"]
-        self.weight_b = data["weight_b"]
-        self.weight = weight_types[data["weight_type"]](self.weight_a, self.weight_b)
-        self.ground_truth = np.array(list(map(int, list(data["ground_truth"]))))
+        self.weights = data["weights"]
+        self.ground_truths = data["ground_truths"]
+        self.proportions = data["proportions"]
         self.filepath = data["filepath"]
 
     def simulate(self):
@@ -26,18 +26,30 @@ class Data_Simulator:
         perms = list(permutations(BASE_LIST))
         perm_probs = np.zeros(len(perms))
 
-        # calculate the probabilities of each permutation
-        for i in range(len(perms)):
-            rank_i = perms[i]
-            rank_as_arr = np.array(rank_i)
-            pi_rank = Ranking(len(BASE_LIST), self.weight, rank_as_arr, ground_truth=self.ground_truth)
-            perm_probs[i] = pi_rank.calc_pi_prob()
-        # create list of simulated rankings based on probabilities
-        for i in range(self.num_rankings):
-            all_rankings_data = np.append(all_rankings_data, perms[np.random.choice(range(len(perms)), p=perm_probs)])
+        for i, ground_truth_str in enumerate(self.ground_truths):
+            cur_ground_truth = np.array(list(map(int, list(ground_truth_str))))
+            cur_weight = self.weights[i]
+            cur_proportion = self.proportions[i]
+            cur_a = float(cur_weight[1])
+            cur_b = float(cur_weight[2])
+            cur_weight_obj = self.weight_types[cur_weight[0]](cur_a, cur_b)
+
+            # calculate the probabilities of each permutation
+            for i in range(len(perms)):
+                rank_i = perms[i]
+                rank_as_arr = np.array(rank_i)
+                pi_rank = Ranking(len(BASE_LIST), cur_weight_obj, rank_as_arr, ground_truth=cur_ground_truth)
+                perm_probs[i] = pi_rank.calc_pi_prob()
+
+            # create list of simulated rankings based on probabilities
+            cur_num_rankings = int(cur_proportion * self.num_rankings)
+            for i in range(cur_num_rankings): # TODO maybe change the line below to be more efficient
+                all_rankings_data = np.append(all_rankings_data, perms[np.random.choice(range(len(perms)), p=perm_probs)])
+
 
         # Reshape 1D array to different dimensions (Num_rankings by Number of elements in each ranking) and return
-        all_rankings_data = all_rankings_data.reshape(self.num_rankings, int(np.size(all_rankings_data)/self.num_rankings))
+        actual_num_rankings = int(len(all_rankings_data) / self.num_elements)
+        all_rankings_data = all_rankings_data.reshape((actual_num_rankings, self.num_elements))
 
         if data_summary:
             uniques = np.unique(all_rankings_data, axis=0, return_counts=True)
@@ -45,6 +57,6 @@ class Data_Simulator:
             counts = uniques[1]
             for i in range(len(unique_ranks)):
                 unique_rank_str = " ".join(unique_ranks[i].astype(str))
-                # self.logger.info("Permutation: " + unique_rank_str + "\t" + " Count: "  + str(counts[i]))
+                logging.info("Permutation: %s \t Count: %d" % (unique_rank_str, counts[i]))
 
         return all_rankings_data
